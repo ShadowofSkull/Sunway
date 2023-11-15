ccabf4af82c7122ea5eb4688f413e760ccf01896
ShadowofSkull <zhplana@gmail.com>
2023-11-15 11:49:44 +0800

    slight chg

--- final_proj/js/main.js
+++ final_proj/js/main.js
@@ -1,29 +1,30 @@
-// form validation
-// const contactForm = document.getElementById("contact-form");
-
-// contactForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-
-//     const name = document.getElementById("name")
-//     const email = document.getElementById("email")
-//     const message = document.getElementById("message")
-
-//     if (name.value === "") {
-//         console.log("Name can't be empty")
-//     }
-    
-//     if (email.value === "") {
-//         console.log("Email can't be empty")
-//     }
-
-    
-//     if (message.value === "") {
-//         console.log("Email can't be empty")
-//     }
-
-// })
-
+// Initialise selector
 const nav = document.querySelector("nav");
+const contactForm = document.getElementById("contact-form");
+
+// form validation
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name")
+    const email = document.getElementById("email")
+    const message = document.getElementById("message")
+
+    if (name.value === "") {
+        console.log("Name can't be empty")
+    }
+    
+    if (email.value === "") {
+        console.log("Email can't be empty")
+    }
+
+    
+    if (message.value === "") {
+        console.log("Email can't be empty")
+    }
+
+})
+
 function toggleNav() {
     nav.classList.toggle("hide");
     console.log("clicked");
