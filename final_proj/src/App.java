9aa99074e86a6a316b46f0f24cc30f21a780c585
ShadowofSkull <zhplana@gmail.com>
2023-12-04 09:00:17 +0800

    change final proj files

--- final_proj/src/App.java
+++ final_proj/src/App.java
@@ -29,18 +29,7 @@ public class App {
     // });
 
     // myJFrame.setVisible(true);
-    try {
-      File myObj = new File("final_proj/pickachu.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
+
     // System.out.println(pickachu);
   }
 }
