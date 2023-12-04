571619784cf6131211f832780194843176dc2932
ShadowofSkull <zhplana@gmail.com>
2023-12-04 14:53:41 +0800

    complete merge

--- final_proj/src/Test.java
+++ final_proj/src/Test.java
@@ -3,33 +3,19 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Test {
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
 
         try {
-      File myObj = new File("final_proj/pickachu.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+            File myObj = new File("final_proj/pickachu.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
